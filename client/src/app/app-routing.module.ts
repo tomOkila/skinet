b11a7096d1076441250ead1780437adf3c565d0c
Your Name <you@example.com>
2021-10-15 16:02:44 +0300

    End of Section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from 'src/core/not-found/not-found.component';
+import { ServerErrorComponent } from 'src/core/server-error/server-error.component';
+import { TestErrorComponent } from 'src/core/test-error/test-error.component';
 import { ProductDetailsComponent } from 'src/shop/product-details/product-details.component';
 import { ShopComponent } from 'src/shop/shop.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '',component:HomeComponent},
+  {path: 'test-error',component:TestErrorComponent},
+  {path: 'server-error',component:ServerErrorComponent},
+  {path: 'not-found',component:NotFoundComponent},
   {path: 'shop', loadChildren: () => import('../shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**',redirectTo:'',pathMatch:'full'}
 ];
